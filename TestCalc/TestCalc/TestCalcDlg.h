07f0cd44691f27571a6c91b7142083833291ed24
LeeJungGeun <59906411+LeeJungGeun@users.noreply.github.com>
2020-03-13 16:39:56 +0900

    계산기 기본틀 구현

---
+++ TestCalc/TestCalc/TestCalcDlg.h
@@ -0,0 +1,40 @@
+
+// TestCalcDlg.h : 헤더 파일
+//
+
+#pragma once
+
+
+// CTestCalcDlg 대화 상자
+class CTestCalcDlg : public CDialogEx
+{
+private:
+	int m_value = 0; //숫자저장값
+	char m_step = 0; //확인변수
+	char m_operater_flag = 0; //기호
+// 생성입니다.
+public:
+	CTestCalcDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TESTCALC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+
+};
