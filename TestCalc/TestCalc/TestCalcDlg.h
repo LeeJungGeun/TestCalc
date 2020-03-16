188d3ea5559d6914439cba6aa7bb8c4a2c44f5ad
LeeJungGeun <59906411+LeeJungGeun@users.noreply.github.com>
2020-03-16 10:11:49 +0900

    곱 나눗셈 구현

--- TestCalc/TestCalc/TestCalcDlg.h
+++ TestCalc/TestCalc/TestCalcDlg.h
@@ -1,6 +1,8 @@
 
 // TestCalcDlg.h : 헤더 파일
 //
+#include <vector>
+#include <array>
 
 #pragma once
 
@@ -9,10 +11,20 @@
 class CTestCalcDlg : public CDialogEx
 {
 private:
-	int m_value = 0; //숫자저장값
-	char m_step = 0; //확인변수
-	char m_operater_flag = 0; //기호
-// 생성입니다.
+	char m_operater_flag = 0;
+	char m_step = 0;
+	int m_count = 0;
+	double m_value = 0;
+	std::vector<CString> a;
+	int count = 1;
+	std::array<double, 99> muldi = { 0 };
+	CString starts;
+	double plma = 0;
+	double all = 0;
+	double muldicount = 0;
+	CString ex;
+	CString nulll;
+	// 생성입니다.
 public:
 	CTestCalcDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
 
@@ -21,11 +33,11 @@ public:
 	enum { IDD = IDD_TESTCALC_DIALOG };
 #endif
 
-	protected:
+protected:
 	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+	virtual BOOL OnCommand(WPARAM wParam, LPARAM lParam);
 
-
-// 구현입니다.
+	// 구현입니다.
 protected:
 	HICON m_hIcon;
 
@@ -34,7 +46,9 @@ protected:
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
+
 	DECLARE_MESSAGE_MAP()
-
-
-};
+public:
+	afx_msg void OnBnClickedMultiply();
+	afx_msg void OnBnClickedDivision();
+}
